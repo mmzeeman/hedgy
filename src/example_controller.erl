b39b6c4999e534c87ad046621c826bb608029b61
Maas-Maarten Zeeman <mmzeeman@xs4all.nl>
2013-11-17 12:51:29 +0100

    Thinking about a different, more explicit, way to implement a controller

--- src/example_controller.erl
+++ src/example_controller.erl
@@ -11,3 +11,21 @@ ping(ReqData, Context) ->
 
 to_html(ReqData, Context) -> 
 	{<<"Hello, new world">>, ReqData, Context}.
+
+% %% Alternative controller implementation
+
+% %%
+% call(ping, ReqData, Context) ->
+% 	ping(ReqData, Context);
+
+% %% 
+% call(to_html, ReqData, Context) ->
+% 	to_html(ReqData, Context);
+% 	{<<"Hello, new world">>, ReqData, Context};
+
+% %%
+% call(F, ReqData, Context) ->
+% 	%% Explicit delegation of default values can go here. 
+% 	%% Cleaner than import tricks IMHO.
+% 	%%html_controller_defaults:call(F, ReqData, Context).
+
